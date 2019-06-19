43ce85728e77ac406988274ef59163404de96856
Julio Alves <julioalves.mesquita@gmail.com>
2019-06-19 10:28:32 -0300

    Novos arquivos

---
+++ functions/figure/plot_voltage_lambda_equilibrium.m
@@ -0,0 +1,26 @@
+function plot_voltage_lambda_equilibrium(equilibrium_points, lambda, name, folder)
+%PLOT_VOLTAGE_CURRENT_EQUILIBRIUM Plot Voltage x Current
+%   Makes a Voltage X Current plot from the simulation output.
+%   The image will be saved automatically if the folder is argument 
+%   is given
+
+    configuration = figure_configuration;
+    
+    configuration.save = nargin == 4;
+    
+    if configuration.save
+        configuration.folder_path = folder;
+    end
+    
+    configuration.title = strcat(name, ' - Output x Duty Cycle');
+
+    data.x = lambda(:,1);
+    data.y = equilibrium_points(:,2);
+
+    
+    configuration.ylabel = 'v_o [V]';
+    configuration.xlabel = '\lambda_1';
+    
+    plot_figure(data, configuration);
+end
+
---
+++ pid_project.m
@@ -0,0 +1,51 @@
+%% Initial Setup
+clear; clc; close all;
+
+% folders to create
+image_folder = 'images';
+cache_folder = 'tmp/cache';
+
+[~,~]=mkdir(image_folder);
+image_folder = strcat(image_folder, '/');
+[~,~]=mkdir(cache_folder);
+cache_folder = strcat(cache_folder, '/');
+
+addpath(genpath('functions'))
+addpath(genpath('models'))
+
+Simulink.fileGenControl('set', 'CacheFolder', cache_folder);
+
+%% Simulation Parametersr
+
+% Desired DC-DC converter to use
+% Options can be found in the system directory:
+%   buck
+%   boost
+%   buck_boost
+circuit = boost;
+
+% Lambda used to create the mean system with wich the controle will be
+% designed
+% Value must be a vector where:
+%   sum(lambda) = 1
+lambda = [0.5 0.5];
+
+sys = default_converter_sys(circuit);
+
+%% System Equilibrium Points
+
+[A, B] = calc_sys_lambda(sys, lambda);
+
+C = sys.C{1};
+D = sys.D{1};
+
+sys_ss = ss(A, B, C, D);
+
+s = tf('s');
+sys_tf = tf(sys_ss);
+
+Kp = 13.2;
+Ki = 7.24e03;
+C_pid = Kp+Ki/s;
+
+sisotool(sys_tf, C_pid);
